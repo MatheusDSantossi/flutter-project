29b073df828508ac159782c469a2e773cabecc1a
Matheus Santos <matheus1819.santos20@gmail.com>
2023-07-16 10:26:35 -0300

    Adicionando primeira versão do código

---
+++ flutter_project_mobile/packages/auth_repository/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\mathe\AppData\Local\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Documents\exercicios_faculdade\mobile_stuff\flutter_project\flutter_project_mobile\packages\auth_repository"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
